8b857f79ae298e83d6036bf81b9af20ca0769e59
Samuel Ryc <contact@samuelryc.com>
2024-03-11 21:22:31 -0400

    Progress - some parts still pretty badly coded

--- js/index.js
+++ js/index.js
@@ -1,13 +1,20 @@
-let targetFutureValue = null, years = null, rate = null;
+let targetFutureValue = null, years = null, interestRate = null, presentValue = null;
 
 window.index = 
 {
     init : function() 
     {
-        $("#input-item-full-price").on(
+        $("#input-target-future-value").on(
             "change",
             function(event) {
                 targetFutureValue = $(this).val();
+            }
+        );
+
+        $("#input-present-value").on(
+            "change",
+            function(event) {
+                presentValue = $(this).val();
             }
         );
 
@@ -18,60 +25,63 @@ window.index =
             }
         );
 
-        $("#input-interests-rate").on(
+        $("#input-interest-rate").on(
             "change",
             function(event) {
-                rate = $(this).val();
+                interestRate = $(this).val();
             }
         );
 
         $("#submit").on(
             "click", 
-            function(event) {
+            function() {
                 $("#input-container").hide();
                 $("#results-container").fadeIn(2000);
                 $("#table-stats-1").html("");
                 $("#table-stats-1").html(
                     "<tr>" +
                         "<th>YEAR</th>" +
-                        "<th>PROFIT FROM INTERESTS</th>" +
-                        "<th>INITIAL CASHDOWN</th>" +
+                        "<th>PROFIT</th>" +
+                        "<th>AMOUNT</th>" +
                     "</tr>"
                 );
 
-                rate = interestRate / 100;
+                const rate = interestRate / 100;
                 const futureValue = targetFutureValue * Math.pow(1 + rate, years);
 
-                $("#p-target-full-price").append("Target price" + " " + '<span id="result-target-price">' + commafy(targetFutureValue) + "$" + "</span>");
-                $("#p-interests-gained").append("Profit from interests after" + " " + years + " " + 'years <span id="result-interests">' + commafy(futureValue) + "$" + "</span>");
-                $("#p-interests-rate").append("Interests annual rate" + " " + '<span id="result-interest-rate">' + rate + "%" +"</span>");
-                $("#p-money-invested").append("Initial cashdown needed" + " " + '<span id="result-money-to-save">' + commafy((targetFutureValue - futureValue).toFixed(2)) + "$" + "</span>");
+                $("#p-target-future-value").append("Target amount" + " " + '<span id="result-future-value">' + Number(targetFutureValue).toFixed(2) + " $" + "</span>");
+                $("#p-present-value").append("Initial amount" + " " + '<span id="result-present-value">' + Number(presentValue).toFixed(2) + " $" + "</span>");
+                $("#p-interest-rate").append("Interests annual rate" + " " + '<span id="result-interest-rate">' + interestRate + " %" +"</span>");
+                $("#p-interests-gained").append("Profit from interests after" + " " + years + " " + 'years <span id="result-interests-gained">' + (presentValue * Math.pow(1 + rate, years)).toFixed(2) + " $" + "</span>");
+                
                 $("#table-stats-1").fadeIn(2000);
 
                 for (let year = 1; year <= years; year++) {
                     // Calculate future value for each year
-                    const futureValue = targetFutureValue * Math.pow(1 + rate, year);
+                    const futureValue = presentValue * Math.pow(1 + rate, year);
                 
                     // Calculate difference from target future value
                     const difference = targetFutureValue - futureValue;
                 
                     // Print results with formatting
-                    console.log(`${year}\t$${targetFutureValue.toFixed(2)}\t$${futureValue.toFixed(2)}\t$${difference.toFixed(2)}`);
+                    console.log(targetFutureValue);
+                    console.log(`${year}\t$${Number(targetFutureValue).toFixed(2)}\t$${futureValue.toFixed(2)}\t$${difference.toFixed(2)}`);
                 
                     // Update present value for next year (optional)
                     // This assumes deposits are made annually. You can remove this line if not applicable.
-                    targetFutureValue = futureValue;
-
+                    //targetFutureValue = futureValue;
+                    //const money_remaining_to_goal = difference.toFixed(2) + " $" 
+                    
                     $("#table-stats-1").append(
-                        '<tr class="tr-row-' + row_iterator + '">' +
-                            "<td>" + row_iterator + "</td>" +
-                            "<td>" + commafy(current_interests) + "$" + "</td>" +
-                            "<td>" + commafy(money_remaining_to_goal) + "$" + "</td>" +
+                        '<tr class="tr-row-' + year + '">' +
+                            "<td>" + year + "</td>" +
+                            "<td>" + (Number(futureValue) - Number(presentValue)).toFixed(2) + " $</td>" +
+                            "<td>" + futureValue.toFixed(2) + " $" + "</td>" +
                         "</tr>"
                     );
 
-                    if (row_iterator == item_target_date) {
-                        $(".tr-row-" + row_iterator).css("background-color", "yellow");
+                    if (futureValue > targetFutureValue) {
+                        $(".tr-row-" + year).css("background-color", "yellow");
                     }
                 }
             }
@@ -79,23 +89,8 @@ window.index =
     }
 };
 
-$(document).on(
-    "ready",
+$(document).ready(
     function() {
         window.index.init();
     }
 );
-
-function commafy(num) {
-    let str = num.toString().split(".");
-
-    if (str[0].length >= 5) {
-        str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, "$1 ");
-    }
-
-    if (str[1] && str[1].length >= 5) {
-        str[1] = str[1].replace(/(\d{3})/g, "$1 ");
-    }
-
-    return str.join(".");
-}
